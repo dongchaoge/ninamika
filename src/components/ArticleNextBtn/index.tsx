37a8ca8ec344c45584b70589227d253dcf9e8881
dongcg <dongcg@dustess.com>
2023-04-30 18:53:09 +0800

    feat: split components

--- src/components/ArticleNextBtn/index.tsx
+++ src/components/ArticleNextBtn/index.tsx
@@ -1,20 +1,67 @@
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
+import { useState } from 'react';
 
-interface Props {
-  text: string;
-  onClick: () => void;
+interface Question {
+  dm: string;
+  question: string;
 }
-const ArticleNextBtn = ({ text, onClick }: Props) => {
+interface Props {
+  question?: Question | null;
+  text: string;
+  prev?: string;
+  onClick: (answer?: string) => void;
+  onPrevClick?: () => void;
+}
+const ArticleNextBtn = ({
+  text,
+  question,
+  prev,
+  onClick,
+  onPrevClick,
+}: Props) => {
+  const [answer, setAnawer] = useState('');
   return (
-    <div className="text-right">
-      <Button
-        onClick={onClick}
-        className="w-[200px] h-[60px] text-[30px] mt-4"
-        type="default"
-      >
-        {text}
-      </Button>
-    </div>
+    <>
+      {question && (
+        <div>
+          <div className="text-2xl text-white my-4">
+            <h4>DM: {question.dm}</h4>
+            <h4 className="mt-4">Question: {question.question}</h4>
+          </div>
+        </div>
+      )}
+      <div className="flex items-center mt-8">
+        {prev && (
+          <Button
+            onClick={onPrevClick}
+            className="w-[200px] h-[60px] text-[30px] mt-4"
+            type="default"
+          >
+            {prev}
+          </Button>
+        )}
+        <div className="flex-1">
+          {question && (
+            <>
+              <Input
+                onInput={(e: any) => setAnawer(e.target.value)}
+                className="h-[60px] text-2xl"
+                placeholder="请回答"
+              />
+            </>
+          )}
+        </div>
+        {text && (
+          <Button
+            onClick={() => onClick(answer)}
+            className="w-[200px] h-[60px] text-[30px] ml-4"
+            type="default"
+          >
+            {text}
+          </Button>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/Nina/index.tsx
+++ src/pages/Nina/index.tsx
@@ -3,6 +3,7 @@ import ArticleContent from '@/components/ArticleContent';
 import ArticleNextBtn from '@/components/ArticleNextBtn';
 import ArticleTitle from '@/components/ArticleTitle';
 
+import { message } from 'antd';
 import { useState } from 'react';
 import {
   finallyArticale,
@@ -17,6 +18,33 @@ const HomePage: React.FC = () => {
 
   const partName = ['序章', '第一章', '第二章', '第三章', '终章'];
 
+  const nextBtnName = ['开始调查', '继续', '继续', '继续', ''];
+  const questionList = [
+    null,
+    {
+      dm: '先和旁边的人认知一下吧，然后输入下面这个问题的答案。',
+      question: 'Ninamika演讲那天穿的什么颜色的鞋子？',
+      verify(answer: string | undefined): boolean {
+        return !!answer?.includes('黑色');
+      },
+    },
+    {
+      dm: '先和旁边的人聊聊高中的最后一年吧，然后输入下面这个问题的答案。',
+      question: '比赛那天，Ninamika穿的什么颜色的外套？',
+      verify(answer: string | undefined): boolean {
+        return !!answer?.includes('白色');
+      },
+    },
+    {
+      dm: '先和旁边的人聊聊工作之后的事情，然后输入下面这个问题的答案。',
+      question: '你是谁？',
+      verify(answer: string | undefined): boolean {
+        return !!answer?.includes('inamika');
+      },
+    },
+    null,
+  ];
+
   const partArticle = [
     zeroArticale,
     firstArticale,
@@ -25,20 +53,39 @@ const HomePage: React.FC = () => {
     finallyArticale,
   ];
 
-  const handleNext = (part: number) => {
-    setPart(part);
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const handleNext = (answer: string | undefined, part: number) => {
+    if (!questionList[part] || questionList[part]?.verify(answer)) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+      setPart(part + 1);
+    } else {
+      messageApi.open({
+        type: 'error',
+        content: 'This is an error message',
+      });
+    }
   };
 
   return (
-    <ArticleContainer>
-      <ArticleTitle title={`韩薇娜-${partName[part]}`} />
-      <ArticleContent articleList={partArticle[part]} />
-      <ArticleNextBtn text="开始调查" onClick={() => handleNext(1)} />
-    </ArticleContainer>
+    <>
+      {contextHolder}
+      <ArticleContainer>
+        <ArticleTitle title={`韩薇娜-${partName[part]}`} />
+        <ArticleContent articleList={partArticle[part]} />
+
+        <ArticleNextBtn
+          text={nextBtnName[part]}
+          question={questionList[part]}
+          onClick={(answer) =>
+            handleNext(answer, part < partName.length ? part : 0)
+          }
+        />
+      </ArticleContainer>
+    </>
   );
 };
 
